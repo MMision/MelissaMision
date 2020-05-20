d2426f3532af34cc641044e38ed1bdbe17da489c
MMision <40003240+MMision@users.noreply.github.com>
2020-05-19 22:22:16 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
 $(".open").on("click", function() {
-  $(".popup-overlay, .popup-content").addClass("active");
+  $(".popup-overlay, .popup-content, .popup-background").addClass("active");
 });
 
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
 $(".close, .popup-overlay").on("click", function() {
-  $(".popup-overlay, .popup-content").removeClass("active");
+  $(".popup-overlay, .popup-content, .popup-background").removeClass("active");
 });
+
+function myFunction() {
+  var x = document.getElementsByClassName("popup-overlay");
+  var y = document.getElementsByClassName("cicada");
+  if (x.style.visibility="hidden") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+myFunction();
