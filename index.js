d35c36dae1199c239895c86011e427cbd24aa574
MMision <40003240+MMision@users.noreply.github.com>
2020-05-20 15:58:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,79 @@
-// document.getElementById('hawk').onclick = function() {
-// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
-// $(".open").on("click", function() {
-//   $(".popup-overlay, .popup-content, .popup-background").addClass("active");
-// });
-// }
 
-// document.getElementById('goat').onclick = function() {
-// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
-// $(".open").on("click", function() {
-//   $(".popup-overlay, .popup-content2, .popup-background").addClass("active");
-// });
-// }
+document.getElementById('seagull').onclick = function() {
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".popup-overlay, .popup-content4, .popup-background").addClass("active");
+  var a = document.getElementById("seagullSound"); 
+  a.play(); 
+} 
+document.getElementById('dolphin').onclick = function() {
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".popup-overlay, .popup-content5, .popup-background").addClass("active");
+  var b = document.getElementById("dolphinSound"); 
+  b.play(); 
+} 
 
-// document.getElementById('cicada').onclick = function() {
-// //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-// $(".close, .popup-overlay").on("click", function() {
-//   $(".popup-overlay, .popup-content3, .popup-background").removeClass("active");
-// });
-// }
+document.getElementById('orca').onclick = function() {
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".popup-overlay, .popup-content6, .popup-background").addClass("active");
+  var c = document.getElementById("orcaSound"); 
+  c.play(); 
+} 
+document.getElementById('close4').onclick = function() {
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $(".popup-overlay, .popup-content4, .popup-background").removeClass("active");
+    var a = document.getElementById("seagullSound"); 
+    a.pause(); 
+}
+document.getElementById('close5').onclick = function() {
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $(".popup-overlay, .popup-content5, .popup-background").removeClass("active");
+    var b = document.getElementById("dolphinSound"); 
+    b.pause(); 
+}
+document.getElementById('close6').onclick = function() {
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $(".popup-overlay, .popup-content6, .popup-background").removeClass("active");
+    var c = document.getElementById("orcaSound"); 
+    c.pause(); 
+}
 
-// //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-// $(".close, .popup-overlay").on("click", function() {
-//   $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
-// });
 
 
 
 document.getElementById('hawk').onclick = function() {
 //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
   $(".popup-overlay, .popup-content, .popup-background").addClass("active");
-}
+  var x = document.getElementById("hawkSound"); 
+  x.play(); 
+} 
 
 document.getElementById('goat').onclick = function() {
 //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
   $(".popup-overlay, .popup-content2, .popup-background").addClass("active");
-}
-
+  var y = document.getElementById("goatSound"); 
+  y.play(); 
+} 
 document.getElementById('cicada').onclick = function() {
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
   $(".popup-overlay, .popup-content3, .popup-background").addClass("active");
+  var z = document.getElementById("cicadaSound"); 
+  z.play(); 
 }
-
 document.getElementById('close').onclick = function() {
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-  $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
+  $(".popup-overlay, .popup-content, .popup-background").removeClass("active");
+    var x = document.getElementById("hawkSound");
+    x.pause(); 
 }
 document.getElementById('close2').onclick = function() {
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-  $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
+  $(".popup-overlay, .popup-content2, .popup-background").removeClass("active");
+    var y = document.getElementById("goatSound"); 
+    y.pause(); 
 }
 document.getElementById('close3').onclick = function() {
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
-  $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
+  $(".popup-overlay, .popup-content3, .popup-background").removeClass("active");
+    var z = document.getElementById("cicadaSound"); 
+    z.pause(); 
 }
