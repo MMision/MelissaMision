5ac95d0a52000b99e90b0b4647ed2816c9a018a9
MMision <40003240+MMision@users.noreply.github.com>
2020-05-20 09:59:41 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
-$(".open").on("click", function() {
-  $(".popup-overlay, .popup-content, .popup-background").addClass("active");
-});
+// document.getElementById('hawk').onclick = function() {
+// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+// $(".open").on("click", function() {
+//   $(".popup-overlay, .popup-content, .popup-background").addClass("active");
+// });
+// }
 
+// document.getElementById('goat').onclick = function() {
+// //appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+// $(".open").on("click", function() {
+//   $(".popup-overlay, .popup-content2, .popup-background").addClass("active");
+// });
+// }
+
+// document.getElementById('cicada').onclick = function() {
+// //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+// $(".close, .popup-overlay").on("click", function() {
+//   $(".popup-overlay, .popup-content3, .popup-background").removeClass("active");
+// });
+// }
+
+// //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+// $(".close, .popup-overlay").on("click", function() {
+//   $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
+// });
+
+
+
+document.getElementById('hawk').onclick = function() {
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".popup-overlay, .popup-content, .popup-background").addClass("active");
+}
+
+document.getElementById('goat').onclick = function() {
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+  $(".popup-overlay, .popup-content2, .popup-background").addClass("active");
+}
+
+document.getElementById('cicada').onclick = function() {
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+  $(".popup-overlay, .popup-content3, .popup-background").removeClass("active");
+}
+
+document.getElementById('close').onclick = function() {
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
 $(".close, .popup-overlay").on("click", function() {
-  $(".popup-overlay, .popup-content, .popup-background").removeClass("active");
+  $(".popup-overlay, .popup-content, .popup-content2, .popup-content3, .popup-background").removeClass("active");
 });
-
-function myFunction() {
-  var x = document.getElementsByClassName("popup-overlay");
-  var y = document.getElementsByClassName("cicada");
-  if (x.style.visibility="hidden") {
-    y.style.display = "none";
-  } else {
-    y.style.display = "block";
-  }
 }
-myFunction();
